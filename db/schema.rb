e7778fa3d1a7abf9f03f899076719290f1377a37
Ao-Ruki <souta.8012@gmail.com>
2020-09-13 22:38:53 +0900

    Basic認証を導入

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_10_144634) do
+ActiveRecord::Schema.define(version: 2020_09_08_003226) do
 
   create_table "destinations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "postcode", null: false
