2bb16100cea7d6c9e2ac61156a11cd1c1aa2f740
Ao-Ruki <souta.8012@gmail.com>
2020-09-14 12:32:50 +0900

    ログインしているユーザーだけが、出品ページへ遷移できること

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,22 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index]
+
+  def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+  
+  def create
+  end
+  private
+  def move_to_index
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
+
+
 end
