96f00fcc3b8841fa93e82dcb3c0a7478f40708a0
Ao-Ruki <souta.8012@gmail.com>
2020-09-16 16:25:10 +0900

    商品を保存、エラーハンドリング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,7 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.all
+    @items = Item.order("created_at DESC")
   end
 
   def new
@@ -10,13 +11,24 @@ class ItemsController < ApplicationController
   end
   
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+
   private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :price, :category_id, :item_condition_id, :postage_player_id, :prefecture_id, :preparation_day_id)
+  end
+
   def move_to_index
     unless user_signed_in?
       redirect_to new_user_session_path
     end
   end
-
 
 end
