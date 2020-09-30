95cea65bbd159360b47a67250d3e8b745af77323
Ao-Ruki <souta.8012@gmail.com>
2020-09-30 12:31:19 +0900

    不要なコントローラーの削除、バリデーション、テストコードの追加

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,23 +1,2 @@
 class DestinationsController < ApplicationController
-  def index
-  end
- 
-  def new
-    @destinations = Destinations.new
-  end
- 
-  def create
-   @destinations = Destinations.new(destinations_params)
-   if @destinations.save
-    redirect_to root_path
-  else
-    render :index
-  end
-  end
- 
-  private
- 
-  def destinations_params
-   params.permit(:post_code, :prefecture_id, :city, :addres, :building_name, :phone_number)
-  end
 end
